719a8ba567e3953def2dd91399a607aa77287dac
w68264Ligezka <luk.lig@wp.pl>
2023-09-15 19:44:13 +0200

    po testach z pobieraniem directory

--- test.py
+++ test.py
@@ -1,53 +1,62 @@
 import os
 import time
 import shutil
+from tkinter import filedialog
 
-os.makedirs('Porządek')
+
+if not os.path.exists('Porządek'):
+    os.makedirs('Porządek')
+
 
 location = 'D:\\Śmietnik\Testowy — kopia'
 locationNew = os.getcwd()
 # location = os.getcwd()
 # print(location)
 
-def rozszerzenie(oneFile):
-    return oneFile.split('.')[-1]
+# a= filedialog.askdirectory()
+
+
+
+
+# def rozszerzenie(oneFile):
+#     return oneFile.split('.')[-1]
  
 
-def check(file,loc):
-    way=loc+"\\"+ file
-    if os.path.isdir(way):
-        main(way)
-    else:
-        wgraj(way,loc)
+# def check(file,loc):
+#     way=loc+"\\"+ file
+#     if os.path.isdir(way):
+#         main(way)
+#     else:
+#         wgraj(way,loc)
 
 
 
-def wgraj(file,locFrom,loc=locationNew):
-    rozszerzenie(file)
-    loc = loc+"\\"+folderName
-    folderName = 'rozszerzenia %s' % (rozszerzenie)
-    if not os.path.exists(loc):
-        os.makedirs(loc)
-    shutil.move(locFrom,loc)
+# def wgraj(file,locFrom,loc=locationNew):
+#     rozszerzenie(file)
+#     loc = loc+"\\"+folderName
+#     folderName = 'rozszerzenia %s' % (rozszerzenie)
+#     if not os.path.exists(loc):
+#         os.makedirs(loc)
+#     shutil.move(locFrom,loc)
     
     
 
-def dateCheck(fileName):
-    data = os.path.getctime(fileName)
-    dataTranslated = time.ctime(data)
-    el1 = dataTranslated[4:7]
-    el2 = dataTranslated[20:24]
-    return [el1,el2]
+# def dateCheck(fileName):
+#     data = os.path.getctime(fileName)
+#     dataTranslated = time.ctime(data)
+#     el1 = dataTranslated[4:7]
+#     el2 = dataTranslated[20:24]
+#     return [el1,el2]
 
 
-def main(loc):
-    plikiPrzed= os.listdir(loc)
+# def main(loc):
+#     plikiPrzed= os.listdir(loc)
 
-    for i in plikiPrzed:
-        check(i,loc)
+#     for i in plikiPrzed:
+#         check(i,loc)
 
 
-    
+
 
 
 
