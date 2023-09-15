1d22fa871bc7ad74cb32a076b21cd371a387322d
w68264Ligezka <luk.lig@wp.pl>
2023-09-15 19:34:01 +0200

    first progres moving done

--- test.py
+++ test.py
@@ -1,15 +1,34 @@
 import os
 import time
+import shutil
+
+os.makedirs('Porządek')
+
+location = 'D:\\Śmietnik\Testowy — kopia'
+locationNew = os.getcwd()
+# location = os.getcwd()
+# print(location)
 
 def rozszerzenie(oneFile):
-    temp = oneFile.split('.')[-1]
-    return + temp
+    return oneFile.split('.')[-1]
+ 
+
+def check(file,loc):
+    way=loc+"\\"+ file
+    if os.path.isdir(way):
+        main(way)
+    else:
+        wgraj(way,loc)
 
 
-def wgraj(rozszerzenie):
-    pathname = 'rozszerzenia %s' % (rozszerzenie)
-    if not os.path.exists(pathname):
-        os.makedirs(pathname)
+
+def wgraj(file,locFrom,loc=locationNew):
+    rozszerzenie(file)
+    loc = loc+"\\"+folderName
+    folderName = 'rozszerzenia %s' % (rozszerzenie)
+    if not os.path.exists(loc):
+        os.makedirs(loc)
+    shutil.move(locFrom,loc)
     
     
 
@@ -21,8 +40,16 @@ def dateCheck(fileName):
     return [el1,el2]
 
 
+def main(loc):
+    plikiPrzed= os.listdir(loc)
 
-wyn = dateCheck("C:\\Users\lukasz.ligezka\Downloads\Dziennik praktyk.docx")
+    for i in plikiPrzed:
+        check(i,loc)
 
 
-print(wyn)
+    
+
+
+
+
+
