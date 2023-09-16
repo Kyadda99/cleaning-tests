2eac44d426ebb14546d43947ed97768e401e9c12
w68264Ligezka <luk.lig@wp.pl>
2023-09-16 14:33:43 +0200

    finished 1.0

--- test.py
+++ test.py
@@ -3,62 +3,119 @@ import time
 import shutil
 from tkinter import filedialog
 
-
-if not os.path.exists('Porządek'):
-    os.makedirs('Porządek')
-
-
-location = 'D:\\Śmietnik\Testowy — kopia'
-locationNew = os.getcwd()
-# location = os.getcwd()
-# print(location)
-
-# a= filedialog.askdirectory()
+location = filedialog.askdirectory()
+locationNew = location.split('/')[-1]
+locationNew = location.replace(locationNew,'')
+print (locationNew)
 
 
 
 
-# def rozszerzenie(oneFile):
-#     return oneFile.split('.')[-1]
+def rozszerzenie(oneFile):
+    return oneFile.split('.')[-1]
  
 
-# def check(file,loc):
-#     way=loc+"\\"+ file
-#     if os.path.isdir(way):
-#         main(way)
-#     else:
-#         wgraj(way,loc)
+def check(file,loc):
+    way=loc+"/"+ file
+    # print("        way  "+way)
+    boolean = os.path.isfile(way)
+    if not boolean:
+        main(way)
+    else:
+        wgraj(way,loc)
 
 
 
-# def wgraj(file,locFrom,loc=locationNew):
-#     rozszerzenie(file)
-#     loc = loc+"\\"+folderName
-#     folderName = 'rozszerzenia %s' % (rozszerzenie)
-#     if not os.path.exists(loc):
-#         os.makedirs(loc)
-#     shutil.move(locFrom,loc)
+def wgraj(file,locFrom,loc=locationNew):
+    end= rozszerzenie(file)
+    folderName = 'rozszerzenia %s' % (end)
+    # print(folderName)
+    loc = loc+"Porządek/"+folderName
+    # print("        loc from  "+file)
+    # print('             loc  '+loc)
+    if not os.path.exists(loc):
+        os.makedirs(loc)
+    shutil.move(file,loc)
+    
+def dateMove(date,loc):
+    print (date)
+    locDate = loc.split('/')[-1]
+    locDate = loc.replace(locDate,'')
+    locDate = locDate +'/'+ transDate(date)
+    if not os.path.exists(locDate):
+        os.makedirs(locDate)
+    shutil.move(loc,locDate)
+
+def transDate(date):
+    month=date[0]
+    year=' '+ date[1]
+    if month == 'Jan':
+        return 'Styczeń'+ year
+    elif month == 'Feb':
+        return 'Luty'+ year
+    elif month == 'Mar':
+        return 'Marzec'+ year
+    elif month == 'Apr':
+        return 'Kwiecień'+ year
+    elif month == 'May':
+        return 'Maj'+ year
+    elif month == 'Jun':
+        return 'Czerwiec'+ year
+    elif month == 'Jul':
+        return 'Lipiec'+ year
+    elif month == 'Aug':
+        return 'Sierpień'+ year
+    elif month == 'Sep':
+        return 'Wrzesień'+ year
+    elif month == 'Oct':
+        return 'Paźdźernik'+ year
+    elif month == 'Nov':
+        return 'Listopad'+ year
+    else:
+        return 'Grudzień'+ year
     
     
 
-# def dateCheck(fileName):
-#     data = os.path.getctime(fileName)
-#     dataTranslated = time.ctime(data)
-#     el1 = dataTranslated[4:7]
-#     el2 = dataTranslated[20:24]
-#     return [el1,el2]
+    
+
+def dateCheck(fileName):
+    data = os.path.getctime(fileName)
+    dataTranslated = time.ctime(data)
+    el1 = dataTranslated[4:7]
+    el2 = dataTranslated[20:24]
+    return [el1,el2]
 
 
-# def main(loc):
-#     plikiPrzed= os.listdir(loc)
+def main(loc):
+    plikiPrzed= os.listdir(loc)
+    
+    for i in plikiPrzed:
+        check(i,loc)
+    os.rmdir(loc)
 
-#     for i in plikiPrzed:
-#         check(i,loc)
+def datySort(loc):
+    dirList=os.listdir(loc)
+    print(dirList)
+    for i in dirList:
+        dirPath=loc+'/'+i
+        newDirList=os.listdir(dirPath)
+        print(newDirList)
+        for j in newDirList:
+            filePath=dirPath+'/'+j
+            datefile=dateCheck(filePath)
+            dateMove(datefile,filePath)
+        
 
 
 
 
+def all(loc,locNew):
+    main(loc)
+    locNew=locNew+'/Porządek'
+    datySort(locNew)
 
+
+all(location,locationNew)
 
 
 
