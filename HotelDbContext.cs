0d849fdfea0b1aace4aa76eb1c13053ad9a3025d
Kyadda99 <127680862+Kyadda99@users.noreply.github.com>
2024-06-29 08:33:47 +0200

    Add files via upload

---
+++ HotelDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ST.Hotel.Entities;
+using System.Net.Sockets;
+using Microsoft.Extensions.Configuration;
+using System;
+namespace ST.Hotel
+{
+    public class HotelDbContext : DbContext
+    {
+        private IConfiguration _configuration { get; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+       
+
+        public HotelDbContext(IConfiguration configuration)
+          : base()
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"server = 10.200.2.28; Database = cinema-dev-w68264; User Id = stud; Password =wsiz; ",
+                       //options.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;database=cinema-dev1;trusted_connection=true;",
+                       //  options.UseSqlServer("",
+                       x => x.MigrationsHistoryTable("__EFMigrationsHistory", "Cinema"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using ST.Hotel.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHotelServices();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Reservation}/{action=Index}/{id?}");
+
+app.Run();
